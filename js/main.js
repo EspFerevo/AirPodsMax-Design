57f4b3e6c6dd8768a974e2d0e575ea86bbfe63d0
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2025-02-19 18:54:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const chooseColor = document.querySelectorAll(".choose-color__btn");
-const contentItem = document.querySelectorAll(".content-item ");
+const contentItem = document.querySelectorAll(".content-item");
 
 chooseColor.forEach(function (el) {
   el.addEventListener("click", open);
@@ -7,10 +7,25 @@ chooseColor.forEach(function (el) {
 
 function open(evt) {
   const target = evt.currentTarget;
+  const btn = target.dataset.button;
 
+  const contentActive = document.querySelectorAll(`.${btn}`);
+
+  // Убираем активный класс у всех кнопок
   chooseColor.forEach(function (item) {
     item.classList.remove("choose-color__btn--active");
   });
 
+  // Добавляем активный класс на выбранную кнопку
   target.classList.add("choose-color__btn--active");
+
+  // Убираем активный класс у всех изображений
+  contentItem.forEach(function (item) {
+    item.classList.remove("content-item__active");
+  });
+
+  // Добавляем активный класс нужным изображениям
+  contentActive.forEach(function (item) {
+    item.classList.add("content-item__active");
+  });
 }
